ac82ef14b9224eb72f781c7cad3c1138809f57af
XinRan Zhang <zhangxinranca16@163.com>
2021-07-16 13:10:03 +0800

    Release

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('home_page', views.home_page, name='home_page'),
+    path('choose_a_theme', views.choose_a_theme, name='choose_a_theme'),
+    path('free_drawing', views.free_drawing, name='free_drawing'),
+    path('my_gallary', views.my_gallary, name='my_gallary'),
+    path('drawing', views.drawing, name='drawing'),
+    path('original_drawing', views.original_drawing, name='original_drawing')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+def home_page(request):
+    return render(request, 'coloring/home_page.html')
+def choose_a_theme(request):
+    return render(request, 'coloring/choose_a_theme.html')
+def free_drawing(request):
+    return render(request, 'coloring/free_drawing.html')
+def my_gallary(request):
+    return render(request, 'coloring/my_gallary.html')
+def drawing(request):
+    return render(request, 'coloring/drawing.html')
+def original_drawing(request):
+    return render(request, 'coloring/original_drawing.html')
